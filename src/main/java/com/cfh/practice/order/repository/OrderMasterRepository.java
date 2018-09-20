23a9296a0002a2b3ce207549cc88fdf4e2277c11
tinysakura <2796191719@qq.com>
2018-09-20 20:26:39 +0800

    基础结构构建完成

---
+++ src/main/java/com/cfh/practice/order/repository/OrderMasterRepository.java
@@ -0,0 +1,11 @@
+package com.cfh.practice.order.repository;
+
+import com.cfh.practice.order.dataobject.OrderMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 廖师兄
+ * 2017-12-10 16:11
+ */
+public interface OrderMasterRepository extends JpaRepository<OrderMaster, String> {
+}
---
+++ src/main/java/com/cfh/practice/order/util/KeyUtil.java
@@ -0,0 +1,22 @@
+package com.cfh.practice.order.util;
+
+import java.util.Random;
+
+/**
+ * Created by 廖师兄
+ * 2017-12-10 16:57
+ */
+public class KeyUtil {
+
+    /**
+     * 生成唯一的主键
+     * 格式: 时间+随机数
+     * 实际生产过程中不能使用这种方式生成随机数
+     */
+    public static synchronized String genUniqueKey() {
+        Random random = new Random();
+        Integer number = random.nextInt(900000) + 100000;
+
+        return System.currentTimeMillis() + String.valueOf(number);
+    }
+}
